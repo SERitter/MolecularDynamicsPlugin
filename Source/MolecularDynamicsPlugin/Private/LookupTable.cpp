af3f4e2e8777cbaa8e0285a29b51c46f98c0412e
Shawn Ritter <83777884+SERitter@users.noreply.github.com>
2021-09-04 21:51:03 -0600

    Corrected include statement error

--- Source/MolecularDynamicsPlugin/Private/LookupTable.cpp
+++ Source/MolecularDynamicsPlugin/Private/LookupTable.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "LookupTable.h"
 #include "Misc/CString.h"
 #include "Misc/FileHelper.h"
-#include "LookupTable.h"
 
 ULookupTable::ULookupTable() {
     // Empty default constructor.
